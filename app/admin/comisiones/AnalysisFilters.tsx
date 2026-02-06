bb5e513cde0a16deac261f8f783a494ab02864e6
Devir G. <gonzalezdevir@gmail.com>
2026-02-05 17:38:33 -0700

    9999

--- app/admin/comisiones/AnalysisFilters.tsx
+++ app/admin/comisiones/AnalysisFilters.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import { useRouter } from "next/navigation";
 
 import { cn } from "@/lib/utils";
